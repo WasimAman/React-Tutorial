9e0428c8da14fb1c88d7bdfee9518ccf079abbb1
WasimAman <wasimaman65@gmail.com>
2024-04-23 11:50:39 +0530

    adding more features in TextUtils

--- 06TextUtils/src/App.jsx
+++ 06TextUtils/src/App.jsx
@@ -1,11 +1,11 @@
-import TextFrom from "../../my-app/src/components/TextFrom"
+import TextFrom from "./components/TextFrom"
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
       <Navbar title = "TextUtils"aboutText = "About Us" homeText = "Home"/>
-      <TextFrom/>
+      <TextFrom heading ="Enter Here"/>
     </>
   )
 }
--- 06TextUtils/src/components/TextFrom.jsx
+++ 06TextUtils/src/components/TextFrom.jsx
@@ -1,24 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 function TextFrom(props) {
-    const [text,setText] = useState()
+  const [text, setText] = useState("");
+
+  const changeToUpperCase = () => {
+      
+      setText(text.toUpperCase());
+  };
+
+  const changeToLowerCase = () => {
+      setText(text.toLowerCase());
+  };
+  const handleOnChange = (event) => {
     
-    const changeToUpperCase = ()=>{
-        setText(text.toUpperCase())
+    setText(event.target.value);
+    console.log(event.target.value);
+    console.log(event.target);
+  };
+
+  const getNoOfWords = (word)=>{
+    if(text===""){
+        return 0;
     }
-    
-    const handleOnChange = (event)=>{
-        setText(event.target.value)
-    }
-    return (
-        <div className="container">
-            <div className="mb-3 my-3">
-                <h4>{props.heading}</h4>
-                <textarea className="form-control" id="text-form" rows="6" value={text} onChange={handleOnChange} placeholder="Enter Here"></textarea>
-            </div>
-            <button id="submit-btn" className="btn btn-primary" onClick={changeToUpperCase}>UpperCase</button>
+    return word.trim().split(" ").length;
+  }
+
+  const getNoOfCharacters = (characters)=>{
+    return characters.length;
+  }
+
+  const getText = ()=>{
+    document.querySelector("#your-text").innerHTML = text;
+  }
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3 my-3">
+          <h4>{props.heading}</h4>
+          <textarea
+            className="form-control"
+            id="text-form"
+            rows="6"
+            value={text}
+            onChange={handleOnChange}
+            placeholder="Enter Here"
+          ></textarea>
         </div>
-    );
+        <button
+          id="submit-btn"
+          className="btn btn-primary mx-1"
+          onClick={changeToUpperCase}
+        >
+          UpperCase
+        </button>
+        <button
+          id="submit-btn"
+          className="btn btn-primary mx-1"
+          onClick={changeToLowerCase}
+        >
+          LowerCase
+        </button>
+        <button
+          id="submit-btn"
+          className="btn btn-primary mx-1"
+          onClick={getText}
+        >
+          GetYourText
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Summary</h1>
+        <h4>No of words:- {getNoOfWords(text)} </h4>
+        <h4>No of character :- {getNoOfCharacters(text)}</h4>
+        <h4>Time to read:- {(getNoOfWords(text)/300).toFixed(2)} minutes (an Average person takes 1 minute to read 300 words)</h4>
+
+        <h4 className="mt-5">Your Written Text :- </h4>
+        <p id="your-text"></p>
+      </div>
+    </>
+  );
 }
 
 export default TextFrom;
