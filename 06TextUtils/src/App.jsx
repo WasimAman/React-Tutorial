294fc0a5321c438469190c1f1dd193d19ba05202
WasimAman <wasimaman65@gmail.com>
2024-04-23 11:00:31 +0530

    adding textUtils

---
+++ 06TextUtils/src/App.jsx
@@ -0,0 +1,13 @@
+import TextFrom from "../../my-app/src/components/TextFrom"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar title = "TextUtils"aboutText = "About Us" homeText = "Home"/>
+      <TextFrom/>
+    </>
+  )
+}
+
+export default App
